985cca4f4ab1626180bf5e5181019585be84f1af
GreedoRen <greedo.it@gmail.com>
2019-05-03 20:05:11 +0300

    Scream Dialog

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -6,6 +6,7 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 import PropTypes from 'prop-types';
 import MyButton from '../util/MyButton';
 import DeleteScream from './DeleteScream';
+import ScreamDialog from './ScreamDialog';
 
 // MUI stuff
 import Card from '@material-ui/core/Card';
@@ -94,6 +95,7 @@ class Scream extends Component {
 						<ChatIcon color='primary' />
 					</MyButton>
 					<span>{commentCount} comments</span>
+					<ScreamDialog screamId={screamId} userHandle={userHandle} />
 				</CardContent>
 			</Card>
 		);
--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -7,7 +7,9 @@ import {
 	LOADING_UI,
 	SET_ERRORS,
 	CLEAR_ERRORS,
-	POST_SCREAM
+	POST_SCREAM,
+	STOP_LOADING_UI,
+	SET_SCREAM
 } from '../types';
 import axios from 'axios';
 
@@ -28,6 +30,20 @@ export const getScream = () => (dispatch) => {
 				payload: []
 			});
 		});
+};
+
+export const getScreamOne = (screamId) => (dispatch) => {
+	dispatch({ type: LOADING_UI });
+	axios
+		.get(`/scream/${screamId}`)
+		.then((res) => {
+			dispatch({
+				type: SET_SCREAM,
+				payload: res.data
+			});
+			dispatch({ type: STOP_LOADING_UI });
+		})
+		.catch((err) => console.log(err));
 };
 
 // Post a scream
